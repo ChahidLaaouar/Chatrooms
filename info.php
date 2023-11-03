4bb2c5ff6c4f1e888633d7cdc81542112af04f97
Chahid <chhdlaaouar@gmail.com>
2023-11-03 21:08:11 +0100

    Info- page afgemaakt

---
+++ info.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Chatrooms</title>
+    <link rel="stylesheet" href="Css/style.css">
+</head>
+<body>
+    <div class="header">
+        <img id="logo" src="img/cr-logo.png">
+        <h1>The Chatrooms</h1>    
+        <form action="index.php">
+            <input type="submit" class="backgroundbutton" value="Home">
+        </form>
+    </div>
+    
+
+    <div class='container'>
+        <h4>Welcome to The Chatrooms - Your Anonymous Social Hub</h4>
+        <p>
+            Are you looking for a platform to connect with new people, share your thoughts, and engage in meaningful conversations?
+            Look no further! Chatrooms is here to provide you with a safe, friendly, and anonymous space to interact with up to 8 strangers simultaneously on a variety of topics.
+        </p>
+        <br>
+
+        <h4>What Is Chatrooms?</h4>
+        <p>
+            Chatrooms is an online social platform designed to bring people together for enjoyable conversations. 
+            Whether you're looking to make new friends, discuss your interests, or vent to someone about your hardships, our chat rooms are the ideal place for you.
+        </p>
+        <br>
+
+        <h4>Join the Conversation Today!</h4>
+        <p>
+            Whether you're passionate about hobbies, curious about life's complexities, seeking valueable advice, or just looking for a friendly chat, Chatrooms has a place for you.
+            Are you ready to explore a place of anonymous, but friendly conversations?
+            Start connecting and join our maze of conversations today!  
+        </p>
+        <br>
+
+        <form action="index.php">
+            <input type="submit" class="button" value="Start Chatting!">
+        </form>
+        
+
+    </div>  
+    
+</body>
+</html>
